dae892cbbcdea4bbbb8d8539313a14edfb85d806
Felipe Zêba <51774420+felipe-zg@users.noreply.github.com>
2020-07-14 15:20:52 -0300

    animating the user gesture

--- src/components/Deck/index.js
+++ src/components/Deck/index.js
@@ -4,6 +4,7 @@ import { View, Animated, PanResponder } from "react-native";
 import { Container } from "./styles";
 
 const Deck = ({ data, renderItem }) => {
+  const position = useRef(new Animated.ValueXY()).current;
   const panResponder = useRef(
     PanResponder.create({
       //What are we touchibg
@@ -11,13 +12,13 @@ const Deck = ({ data, renderItem }) => {
       // How the gesture changes
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => {
-        console.log(gesture);
+        position.setValue({ x: gesture.dx, y: gesture.dy });
       },
       onPanResponderRelease: (event, gesture) => {},
     })
   ).current;
   return (
-    <Container {...panResponder.panHandlers}>
+    <Container style={position.getLayout()} {...panResponder.panHandlers}>
       {data.map((item) => renderItem(item))}
     </Container>
   );
