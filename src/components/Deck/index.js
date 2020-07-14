1ebc53659670e746d895eca7443f7472449a2947
Felipe Zêba <51774420+felipe-zg@users.noreply.github.com>
2020-07-14 15:06:11 -0300

    PanResponder handlers on Deck components

--- src/components/Deck/index.js
+++ src/components/Deck/index.js
@@ -1,10 +1,26 @@
-import React from "react";
-import { View, Animated } from "react-native";
+import React, { useRef } from "react";
+import { View, Animated, PanResponder } from "react-native";
 
 import { Container } from "./styles";
 
 const Deck = ({ data, renderItem }) => {
-  return <Container>{data.map((item) => renderItem(item))}</Container>;
+  const panResponder = useRef(
+    PanResponder.create({
+      //What are we touchibg
+      // What components is handling the Touch
+      // How the gesture changes
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        console.log(gesture);
+      },
+      onPanResponderRelease: (event, gesture) => {},
+    })
+  ).current;
+  return (
+    <Container {...panResponder.panHandlers}>
+      {data.map((item) => renderItem(item))}
+    </Container>
+  );
 };
 
 export default Deck;
